71cd09c31a03fbb55db635245363d53e0c1f157c
biliww <843145936@qq.com>
2026-02-09 15:32:02 +0800

    Initial Commit

---
+++ MacRightClick/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MacRightClick
+//
+//  Created by 王鹏龙 on 2026/2/9.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ MacRightClick/MacRightClickApp.swift
@@ -0,0 +1,32 @@
+//
+//  MacRightClickApp.swift
+//  MacRightClick
+//
+//  Created by 王鹏龙 on 2026/2/9.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MacRightClickApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
